d269e4af3084b6fabb7bf74c947d08f3b0be3778
jyoti <jostring9199@gmail.com>
2021-05-12 20:49:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import { storage } from "./firebase";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audio: null,
+      url: "",
+      progress: 0
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleUpload = this.handleUpload.bind(this);
+  }
+  handleChange = (e) => {
+    if (e.target.files[0]) {
+      const audio = e.target.files[0];
+      this.setState(() => ({ audio }));
+    }
+  };
+  handleUpload = () => {
+    const { audio } = this.state;
+    const uploadTask = storage.ref(`audioFile/${audio.name}`).put(audio);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+      
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        this.setState({ progress });
+      },
+      (error) => {
+      
+        console.log(error);
+      },
+      () => {
+      
+        storage
+          .ref("images")
+          .child(audio.name)
+          .getDownloadURL()
+          .then((url) => {
+            console.log(url);
+            this.setState({ url });
+          });
+      }
+    );
+  };
+  render() {
+    const style = {
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center"
+    };
+    return (
+      <div style={style}>
+        <progress value={this.state.progress} max="100" />
+        <br />
+        <input type="file" onChange={this.handleChange} />
+        <button onClick={this.handleUpload}>Upload</button>
+        <br />
+        <img
+          src={this.state.url }
+          alt="Uploaded  audio"
+        
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
